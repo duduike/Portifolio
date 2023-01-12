329ed7dfe3319293d18a48dbf24ccce2312518d4
duduike <eduardo.happ@yahoo.com>
2023-01-12 13:18:54 -0300

    Improvements in the right scroll ing of the carousel

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -4,27 +4,33 @@
 //3 Pegar botão - querySelector (Consulta de seletor)
 //4 Saber que está sendo clicado o botao
 
-//5 Acessar a janela
-//6 Pegar HTML todo
-//7 Pegar elements
-//8 Mover o elements para direita
-//9 
 
 //const cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura
 //let uma variavel que pode mudar no futuro
 
               //1-------2--------3------------------------------------    
-const btnRight = /*window*/document.querySelector('.button-arrow.-right');
-const btnLeft  = document.querySelector('.button-arrow.-left');
-const elements = document.querySelector('.elements');
-let pixels = 0
+    const btnRight = /*window*/ document.querySelector('.button-arrow.-right');
+    const btnLeft = document.querySelector('.button-arrow.-left');
+    const elements = document.querySelector('.elements');
+    let pixels = 0
+    let totalWidth = elements.scrollWidth;
 
-btnRight.addEventListener('click', function() {
-    pixels = pixels + 100;
-    elements.style = `transform: translateX(${pixels}px)`;
-})
+    btnRight.addEventListener('click', function () {
+        pixels = pixels + 100;
+        elements.style = `transform: translateX(${pixels}px)`;
 
-btnLeft.addEventListener('click', function() {
-    pixels = pixels - 100;
-    elements.style = `transform: translateX(${pixels}px)`;
-})
+        if (pixels > totalWidth - 200) {
+            // pixels = 100 - totalWidth;
+            pixels = 0 - 100;
+
+        }
+    })
+
+    btnLeft.addEventListener('click', function () {
+        pixels = pixels - 100;
+        elements.style = `transform: translateX(${pixels}px)`;
+
+        if (pixels > (totalWidth - (totalWidth - totalWidth))) {
+            pixels = 0 + 100;
+        }
+    })
