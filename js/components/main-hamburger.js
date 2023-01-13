046a01c324b2e7ca02866984d4e7126cf52a63da
duduike <eduardo.happ@yahoo.com>
2023-01-13 03:45:25 -0300

    Create the function for close hamburger menu if click on link

--- js/components/main-hamburger.js
+++ js/components/main-hamburger.js
@@ -11,3 +11,9 @@ document.querySelector('.icon-close').addEventListener('click', function(){
     document.querySelector('.icon-menu').style.display = 'block';
     document.querySelector('.icon-close').style.display = 'none';
 });
+
+document.querySelector('.links').addEventListener('click', function(){
+    document.querySelector('#menu').style.display = 'none';
+    document.querySelector('.icon-menu').style.display = 'block';
+    document.querySelector('.icon-close').style.display = 'none';
+});
