dcd8d0ec05dc1a5d508178bb0be1a5f3ecaf298c
duduike <eduardo.happ@yahoo.com>
2023-01-13 01:06:59 -0300

    Create the hamburger menu for devices mobile

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -34,3 +34,20 @@
             pixels = 0 + 100;
         }
     })
+
+
+
+
+    document.querySelector('.icon-menu').addEventListener('click', function(){
+        document.querySelector('.menu').style.display = 'block';
+        document.querySelector('.icon-menu').style.display = 'none';
+        document.querySelector('.icon-close').style.display = 'block';
+    });
+    
+    document.querySelector('.icon-close').addEventListener('click', function(){
+        document.querySelector('.menu').style.display = 'none';
+        document.querySelector('.icon-menu').style.display = 'block';
+        document.querySelector('.icon-close').style.display = 'none';
+    });
+
+
---
+++ js/components/main-hamburger.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector('.icon-menu').addEventListener('click', function(){
+    document.querySelector('.menu').style.display = 'block';
+    document.querySelector('.icon-menu').style.display = 'none';
+    document.querySelector('.icon-close').style.display = 'block';
+});
+
+document.querySelector('.icon-close').addEventListener('click', function(){
+    document.querySelector('.menu').style.display = 'none';
+    document.querySelector('.icon-menu').style.display = 'block';
+    document.querySelector('.icon-close').style.display = 'none';
+});
