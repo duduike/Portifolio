167d17a53de40cea0f396c24e0551ecfe1e600a2
duduike <eduardo.happ@yahoo.com>
2023-01-13 03:26:49 -0300

    Create Js for hamburger menu

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -34,20 +34,3 @@
             pixels = 0 + 100;
         }
     })
-
-
-
-
-    document.querySelector('.icon-menu').addEventListener('click', function(){
-        document.querySelector('.menu').style.display = 'block';
-        document.querySelector('.icon-menu').style.display = 'none';
-        document.querySelector('.icon-close').style.display = 'block';
-    });
-    
-    document.querySelector('.icon-close').addEventListener('click', function(){
-        document.querySelector('.menu').style.display = 'none';
-        document.querySelector('.icon-menu').style.display = 'block';
-        document.querySelector('.icon-close').style.display = 'none';
-    });
-
-
