35abe0a8197543f28b895334d77e074878854105
duduike <eduardo.happ@yahoo.com>
2023-01-18 23:32:06 -0300

    Create the carousel with slick-carousel and refatored code

--- js/components/main.js
+++ js/components/main.js
@@ -1,4 +1,4 @@
-
+// menu
 
 document.querySelector('.icon-menu').addEventListener('click', function(){
     document.querySelector('#menu').style.display = 'block';
@@ -17,3 +17,40 @@ document.querySelector('.links').addEventListener('click', function(){
     document.querySelector('.icon-menu').style.display = 'block';
     document.querySelector('.icon-close').style.display = 'none';
 });
+
+
+// carousel
+
+$(document).ready(function() {
+    $('.carousel').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: true,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+          
+        }
+      }]
+    });
+  });
+
+  
+// $('.carousel').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+  
+// });
