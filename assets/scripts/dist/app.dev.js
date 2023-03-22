1918f6c95c8539ee60692bee2edd1b1d9143654f
duduike <eduardo.happ@yahoo.com>
2023-03-22 18:45:06 -0300

    Teste

---
+++ assets/scripts/dist/app.dev.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// SWITCH MENU MOBILE/DESKTOP
+document.querySelector('.icon-menu').addEventListener('click', function () {
+  document.querySelector('#menu').style.display = 'block';
+  document.querySelector('.icon-menu').style.display = 'none';
+  document.querySelector('.icon-close').style.display = 'block';
+});
+document.querySelector('.icon-close').addEventListener('click', function () {
+  document.querySelector('#menu').style.display = 'none';
+  document.querySelector('.icon-menu').style.display = 'block';
+  document.querySelector('.icon-close').style.display = 'none';
+});
+document.querySelector('.links').addEventListener('click', function () {
+  document.querySelector('#menu').style.display = 'none';
+  document.querySelector('.icon-menu').style.display = 'block';
+  document.querySelector('.icon-close').style.display = 'none';
+}); // CAROUSEL
+
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    arrows: true,
+    dots: true,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 2,
+        arrows: true,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 747,
+      settings: {
+        slidesToShow: 1,
+        arrows: true,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
